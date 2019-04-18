cb26b5d6a333ef073496875c7924a745be745345
Valerie Remaker <valerie@D-10-18-198-252.dhcp4.washington.edu>
2019-04-18 16:53:48 -0700

    minor fixes

--- app/src/main/java/edu/washington/vremaker/activityspy/MainActivity.kt
+++ app/src/main/java/edu/washington/vremaker/activityspy/MainActivity.kt
@@ -12,6 +12,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Log.i(TAG, "onCreate event fired")
     }
+
+    override fun onStart(){
+        super.onStart()
+        Log.i(TAG, "onStart event fired")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume event fired")
+    }
+    override fun onPause(){
+        super.onPause()
+        Log.i(TAG, "onPause event fired")
+    }
+    override fun onStop(){
+        super.onStop()
+        Log.i(TAG, "onStop event fired")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart event fired")
+    }
+
     override fun onDestroy(){
         super.onDestroy()
         Log.e(TAG, "We're going down, Captain!")
